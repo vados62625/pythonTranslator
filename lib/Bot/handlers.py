9bae7cb58be66db991def629108234e13ce869cb
it-7 <vados62625@gmail.com>
2022-09-07 15:40:52 +0300

    remove token from git

--- lib/Bot/handlers.py
+++ lib/Bot/handlers.py
@@ -2,18 +2,13 @@ from aiogram import types
 from translate import Translator
 
 
-# функция, обрабатывающая команду /start
-
 async def start(message: types.Message):
-
+    
     await message.answer("Привет!\nНапиши мне что-нибудь, а я попробую перевести!")
 
 
-# функция, которая отвечает на сообщение
-
-# текстом
-
 async def echo(message: types.Message):
+    
     with open("chathistory.txt", "a") as file:
         file.write('\n' + message.from_user.username + ' написал: ' + message.text)
     translator = Translator(from_lang='russian', to_lang='english')

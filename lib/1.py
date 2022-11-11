1228751615c33b34396ba352776206808bb41aad
it-7 <vados62625@gmail.com>
2022-11-11 17:35:19 +0300

    remove old files

--- lib/1.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen, PIPE
-command = r"cmd"
-p = Popen(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
-usertext = "python script.py english день \n"
-out, err = p.communicate(usertext)
-print(out)
